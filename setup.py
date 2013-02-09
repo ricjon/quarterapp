d5bde24f2f39c07ee7d1dc2605cf20eb902017f5
haard <fredrik.haard@softhouse.se>
2013-02-09 22:52:24 +0100

    setup.py packages resources and has entrypoint

--- setup.py
+++ setup.py
@@ -1,17 +1,34 @@
 from distutils.core import setup
+import os.path
+
+
+def get_resources():
+    resources = []
+    def assembler(arg, folder, files):
+       resources.append((folder, [os.path.join('quarterapp', folder, f) 
+        for f in files if not os.path.isdir(os.path.join(folder, f))]))
+    os.path.walk('quarterapp/resources', assembler, True)
+    return resources
+
 
 setup(
     name='Quarterapp',
     version='0.0.1',
     author='Markus Eliasson',
     author_email='markus.eliasson@gmail.com',
-    packages=['quarterapp', 'quarterapp.tests', 'quarterapp.resources'],
-    scripts=['bin/quarterapp.py'],
+    packages=['quarterapp', 'quarterapp.tests'],
     url='https://github.com/eliasson/quarterapp',
-    license='LICENSE',
+    license=open('LICENSE').read(),
     description='Personal time management',
     long_description=open('README.md').read(),
     install_requires=[
         "tornado >= 2.4.0"
     ],
+    entry_points = {
+    'console_scripts': [
+        'qapp = quarterapp:main',
+        ]
+    },
+    data_files=get_resources() + [('.', ['./README.md', './LICENSE'])],
+    zip_safe = False,
 )
