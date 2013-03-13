71dfc53235d7378c9328578e912d30d56a3a5845
Markus Eliasson <markus.eliasson@gmail.com>
2013-03-13 22:55:39 +0100

    Updated to version 0.0.11

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def get_resources():
 
 setup(
     name='quarterapp',
-    version='0.0.10',
+    version='0.0.11',
     author='Markus Eliasson',
     author_email='markus.eliasson@gmail.com',
     packages=['quarterapp', 'quarterapp.tests'],
